896ef4fa8fe12b3d555102b58cf06c0a81851596
Ravi Tiwari <ravi.tiwari@walgreens.com>
2025-07-01 23:26:14 -0600

    change formatting

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="voxai",
-    version="0.2.13",
+    version="0.2.14",
     description="Voice-driven AI assistant for real-time transcription and Gemini integration.",
     long_description=long_description,
     long_description_content_type="text/markdown",
